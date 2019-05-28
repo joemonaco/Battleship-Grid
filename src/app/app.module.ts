c28be280ed504f917a78675e9065ecb0d98a74a1
Joe Monaco <s1049491@monmouth.edu>
2019-05-28 09:07:44 -0400

    app module update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,7 +12,10 @@ import { reducer } from "./state-management/reducers/battleship.reducer";
 import { SocketIoModule, SocketIoConfig } from "ngx-socket-io";
 import { GameService } from "src/app/services/game.service";
 
-const config: SocketIoConfig = { url: "192.168.162.190:8000", options: {} };
+const config: SocketIoConfig = {
+  url: "192.168.1.14:8000",
+  options: {}
+};
 
 @NgModule({
   declarations: [AppComponent, GameScreenComponent],
